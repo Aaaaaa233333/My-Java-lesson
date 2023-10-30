a673bf70cfed5b88feb7dfafe01bfa36d6cdc98f
CHUNAN KE <c.ke.22@abdn.ac.uk>
2023-10-30 12:40:16 +0000

    commit message

--- Practical1/HW001.java
+++ Practical1/HW001.java
@@ -1,5 +1,5 @@
 public class HW001 {
 public static void main(String[] args) {
-System.out.println("Today is a wonderfull day!I want to be happy like today forever!");
+System.out.println("Today is a wonderfull day!");
 }
 }
